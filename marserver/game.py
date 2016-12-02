32881e34aba2dfd13c66c62c6f69246bb42daab0
as-f <as-f@users.noreply.github.com>
2016-12-01 23:49:35 -0600

    add some documentation

--- marserver/game.py
+++ marserver/game.py
@@ -1,7 +1,13 @@
+from random import Random
+
 class Game:
     
     def __init__(self, seed, output):
+        self.seed = seed
         self.output = output
+
+        self.random = Random
+        self.random.seed(seed)
 
     def input(self, line):
         self.output(line)
