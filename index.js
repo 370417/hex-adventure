e36fc7a19b2269e1813fa9b526855e95b01f8689
as-f <as-f@users.noreply.github.com>
2016-12-10 15:47:30 -0600

    remove extraneous canvas code

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-function startGame(tiles) {
-    const display = Display(document.getElementById('game'), tiles);
-}
-
-
-loadTiles('tileset.png', tilemap, 18, 24, startGame);
+const display = Display();
