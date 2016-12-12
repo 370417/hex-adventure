fb6c7bbd9e4b29d6ae7e380dfcfb94925c276d53
as-f <as-f@users.noreply.github.com>
2016-12-12 02:05:17 -0600

    schedule: make it all one function

--- schedule.js
+++ schedule.js
@@ -1,49 +1,39 @@
-(() => {    
-    const protoSchedule = {
-        peek() {
-            return this.heap[0];
-        },
+function Schedule() {
+    let time = 0;
+    let heap = [];
 
-
-        push(id, delay) {
-            Heap.push(this.heap, [this.time + delay, id], this.cmp);
-        },
-
-
-        pop() {
-            time, id = Heap.pop(this.heap, this.cmp);
-            this.time = time;
-            return id;
-        },
-
-
-        pushpop(id, delay) {
-            time, newid = Heap.pushpop(this.heap, [this.time + delay, id], this.cmp);
-            this.time = time;
-            return newid;
-        },
-
-
-        cmp(a, b) {
-            if (!a && !b) {
-                return 0;
-            } else if (a[0] - b[0]) {
-                return a[0] - b[0];
-            } else {
-                return this.cmp(a.slice(1), b.slice(1));
-            }
-        },
-    };
-
-
-    function Schedule() {
-        const schedule = {
-            time: 0,
-            heap: [],
-        };
-        return Object.create(protoSchedule, schedule);
+    function peek() {
+        return heap[0];
     }
 
+    function push(id, delay) {
+        Heap.push(heap, [time + delay, id], cmp);
+    }
 
-    this.Schedule = Schedule;
-})();
+    function pop() {
+        [time, id] = Heap.pop(heap, cmp);
+        return id;
+    }
+
+    function pushpop(id, delay) {
+        [time, newid] = Heap.pushpop(heap, [time + delay, id], cmp);
+        return newid;
+    }
+
+    function cmp(a, b) {
+        if (!a && !b) {
+            return 0;
+        } else if (a[0] - b[0]) {
+            return a[0] - b[0];
+        } else {
+            return cmp(a.slice(1), b.slice(1));
+        }
+    }
+
+    return {
+        peek,
+        push,
+        pop,
+        pushpop,
+    };
+}
