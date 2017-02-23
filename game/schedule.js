e06f64786557fc54a6c9ddb4b6051c259d8ce70a
as-f <as-f@users.noreply.github.com>
2017-02-23 09:33:12 -0600

    use es6 classes for schedule

--- game/schedule.js
+++ game/schedule.js
@@ -1,5 +1,28 @@
 // Keep track of future events
 
+class Schedule {
+    constructor() {
+        this._now = 0
+        this._heap = new Heap((a, b) => a.time - b.time || a.id - b.id)
+    }
+
+    push(id, delay) {
+        this._heap.push({time: now + delay, id})
+    }
+
+    pop() {
+        let event = this._heap.pop()
+        this._now = event.time
+        return event.id
+    }
+
+    pushpop(id, delay) {
+        let event = this._heap.pushpop({time: now + delay, id})
+        this._now = event.time
+        return event.id
+    }
+}
+/*
 function Schedule() {
     let now = 0
     let heap = new Heap((a, b) => a.time - b.time || a.id - b.id)
@@ -29,3 +52,4 @@ function Schedule() {
         pushpop,
     }
 }
+*/
