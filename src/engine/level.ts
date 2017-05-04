054c122778f8c9c993f964855c80ecf92cc1bf4d
as-f <as-f@users.noreply.github.com>
2017-05-04 13:37:30 -0500

    add todo

--- src/engine/level.ts
+++ src/engine/level.ts
@@ -77,6 +77,7 @@ export function create(seed: number, playerPos: number): Level {
     }
 
     /** remove groups of 5 or fewer walls */
+    // TODO: investigate potential infinite loop here
     function removeSmallWalls() {
         const visited = new Set()
         forEachInnerPos(pos => {
