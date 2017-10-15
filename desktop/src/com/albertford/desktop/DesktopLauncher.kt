330a05f1dcbbbab01c4f4f735b0181fdc3cce925
as-f <as-f@users.noreply.github.com>
2017-10-14 21:52:40 -0700

    Init project

---
+++ desktop/src/com/albertford/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.albertford.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.albertford.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Hex Adventure";
+		new LwjglApplication(new Game(), config);
+	}
+}
