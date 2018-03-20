4301ca0b1ef717c62deff992b88ec81738ef3eb4
as-f <as-f@users.noreply.github.com>
2018-03-20 08:05:50 -0700

    Display a blank screen

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,38 @@
 extern crate ggez;
+use ggez::{Context, GameResult};
+use ggez::conf::Conf;
+use ggez::event;
+use ggez::event::EventHandler;
+use std::fs::File;
+
+const CONFIG_PATH: &str = "resources/conf.toml";
+
+struct MainState {
+
+}
+
+impl EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let config = load_config();
+    let mut ctx = Context::load_from_conf("hex-adventure", "as-f", config).expect("Failed to load context from configuration.");
+    match event::run(&mut ctx, &mut MainState {}) {
+        Err(e) => println!("Error encountered: {}", e),
+        _ => println!("Game exited cleanly"),
+    }
+}
+
+fn load_config() -> Conf {
+    match File::open(CONFIG_PATH).map(|mut file| Conf::from_toml_file(&mut file)) {
+        Ok(Ok(conf)) => conf,
+        _ => Conf::new(),
+    }
 }
