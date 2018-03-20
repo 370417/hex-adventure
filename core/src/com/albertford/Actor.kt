3544264387827c653e1ef37410d5a3024cb670ca
as-f <as-f@users.noreply.github.com>
2018-03-20 07:32:14 -0700

    Start from scratch in rust

--- core/src/com/albertford/Actor.kt
+++
@@ -1,5 +0,0 @@
-package com.albertford
-
-interface Actor {
-    fun act(gameState: GameState)
-}
--- core/src/com/albertford/display/Panel.kt
+++
@@ -1,7 +0,0 @@
-package com.albertford.display
-
-import com.badlogic.gdx.graphics.g2d.Batch
-
-interface Panel {
-    fun render(batch: Batch)
-}
